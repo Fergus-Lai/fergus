e817b64c7739dcf5cbb741069b2b254f4a489ceb
Fergus Lai <ferguslai724@gmail.com>
2023-10-21 16:08:17 +0100

    Projects Page Compeleted and Fixed Layout

---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { defineCollection, z } from "astro:content";
+const projectCollection = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        startDate: z.string().transform((str) => {
+            const dateList = str.split("/");
+            return new Date(
+                parseInt(dateList[1]),
+                parseInt(dateList[0]) - 1,
+                1
+            );
+        }),
+        endDate: z.string().transform((str) => {
+            const dateList = str.split("/");
+            return new Date(
+                parseInt(dateList[1]),
+                parseInt(dateList[0]) - 1,
+                1
+            );
+        }),
+        techStack: z.string(),
+        github: z.string().url().optional(),
+    }),
+});
+export const collections = {
+    project: projectCollection,
+};
---
+++ src/content/project/aicar.md
@@ -0,0 +1,12 @@
+---
+title: Minitaure Self-Driving Robotics Car
+startDate: 9/2018
+endDate: 7/2019
+techStack: Python, TensorFlow, Computer Vision, Keras, Machine Learning
+---
+
+Participant of a year-long workshop led by Dr George Ng, Associate Lecturer in Data Science at Goldsmiths
+University of London, I built a self-navigating model autonomous vehicle using a Raspberry Pi. And used Python,
+TensorFlow, computer vision and the Keras library to build a machine learning model for the vehicle. This
+experience allowed me to develop a deeper understanding of neural networks and other machine learning
+concepts.
