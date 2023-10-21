8750140ae1cbeef68eaa835c8f039b393ccfdb07
Fergus Lai <ferguslai724@gmail.com>
2023-10-21 16:23:25 +0100

    Updated Layout of Project and added IBM entry

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,14 +11,17 @@ const projectCollection = defineCollection({
                 1
             );
         }),
-        endDate: z.string().transform((str) => {
-            const dateList = str.split("/");
-            return new Date(
-                parseInt(dateList[1]),
-                parseInt(dateList[0]) - 1,
-                1
-            );
-        }),
+        endDate: z
+            .string()
+            .transform((str) => {
+                const dateList = str.split("/");
+                return new Date(
+                    parseInt(dateList[1]),
+                    parseInt(dateList[0]) - 1,
+                    1
+                );
+            })
+            .optional(),
         techStack: z.string(),
         github: z.string().url().optional(),
     }),
---
+++ src/content/project/ibmar.md
@@ -0,0 +1,9 @@
+---
+startDate: 10/2023
+title: IBM AR AI Business Card
+techStack: Unity, Watson, Augmented Reality, Android, C#
+---
+
+As part of a team of six, contributed to the development of an Android phone system to create a 21st century
+business card, with AR integrated to display an AR avatar, and Watson Speech to Text for announcing information
+of the user. Collaborated on the development of the app in Unity.
